ae5cfed440abbaad2a2e755eab79d21cdb5bfdaa
Jesus Guio <jfranciscoguio@protonmail.com>
2024-03-26 14:55:04 -0500

    Score volume component added [#7]

---
+++ Source/SkateGame/Actor/Components/SkateGameScoreVolume.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SkateGameScoreVolume.h"
+
+
+// Sets default values for this component's properties
+USkateGameScoreVolume::USkateGameScoreVolume()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void USkateGameScoreVolume::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void USkateGameScoreVolume::TickComponent(float DeltaTime, ELevelTick TickType,
+                                          FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/SkateGame/Actor/Components/SkateGameScoreVolume.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "SkateGameScoreVolume.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SKATEGAME_API USkateGameScoreVolume : public UBoxComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USkateGameScoreVolume();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
