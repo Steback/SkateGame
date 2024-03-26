94421c5c28238893542523cfda140a4928b40902
Jesus Guio <jfranciscoguio@protonmail.com>
2024-03-26 16:54:15 -0500

    Scorew widget added [#8]

---
+++ Source/SkateGame/Widget/SkateGameScoreWidget.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SkateGameScoreWidget.h"
+
+#include "Components/TextBlock.h"
+
+void USkateGameScoreWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
+
+void USkateGameScoreWidget::OnScoreUpdate(int32 CurrentScore)
+{
+	if (IsValid(ScoreText))
+	{
+		ScoreText->SetText(FText::FromString(FString::Printf(TEXT("%i"), CurrentScore)));
+	}
+}
---
+++ Source/SkateGame/Widget/SkateGameScoreWidget.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SkateGameScoreWidget.generated.h"
+
+class UTextBlock;
+
+/**
+ * 
+ */
+UCLASS()
+class SKATEGAME_API USkateGameScoreWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	virtual void NativeOnInitialized() override;
+	
+public:
+	UFUNCTION()
+	void OnScoreUpdate(int32 CurrentScore);
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UTextBlock> ScoreText;
+};
